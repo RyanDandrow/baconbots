3b8f4b6158cfc6269006459b6de3badeb38962d0
Ryan Dandrow <RyanDandrow@gmail.com>
2015-06-08 11:01:42 -0500

    added play/pause control for music player with Ember Services

--- app/initializers/soundcloud.js
+++ app/initializers/soundcloud.js
@@ -1,11 +1,9 @@
-export function initialize(/* container, application */) {
+export function initialize(container, application) {
   SC.initialize({
     client_id: "b83f2af990b36dad655e4d31a4ad381e"
   });
 
-  //SC.stream("/tracks/28301249", function(sound){
-   //sound.play();
-//});
+  application.inject('component', 'soundcloud', 'service:soundcloud-player');
 }
 
 export default {
